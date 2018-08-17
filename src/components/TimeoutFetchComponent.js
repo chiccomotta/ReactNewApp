a96373cbb44afe0f678def70875adace2014f64b
cristiano motta <cristiano.motta@hotmail.it>
2018-08-17 14:26:58 +0200

    Aborted Fetch Request

--- src/components/TimeoutFetchComponent.js
+++ src/components/TimeoutFetchComponent.js
@@ -11,11 +11,20 @@ class TimeoutFetchComponent extends React.Component {
     }
   }
 
+  abortController = null
+
   componentDidMount() {
+    this.abortController = new window.AbortController()
+
     // Esempio di implementazione di un timeout per una request con fecth
     Promise.race([
-      fetch('http://localhost:3001/api/testapi'),
-      new Promise((_, reject) => setTimeout(() => reject(new Error('Timeout')), 5000))
+      fetch('http://localhost:3001/api/testapi', { signal: this.abortController.signal }),
+      new Promise((_, reject) =>
+        setTimeout(() => {
+          this.abortController.abort()
+          //reject(new Error('Timeout'))
+        }, 3000)
+      )
     ])
       .then(response => {
         return response.text()
@@ -27,12 +36,38 @@ class TimeoutFetchComponent extends React.Component {
       })
   }
 
+  startRequest = () => {
+    this.setState({ message: null, error: null })
+    this.abortController = new window.AbortController()
+
+    fetch('http://localhost:3001/api/testapi', { signal: this.abortController.signal })
+      .then(response => {
+        return response.text()
+      })
+      .then(text => this.setState({ message: text }))
+      .catch(err => {
+        console.log(err)
+        this.setState({ error: 'Request Aborted!' })
+      })
+  }
+
+  abortRequest = () => {
+    this.abortController.abort()
+  }
+
+  reload = () => {
+    window.location.href = '/'
+  }
+
   render() {
     return (
       <div>
         <h2>
           {this.state.message}{' '}
           {this.state.error && <div style={{ color: 'red' }}>{this.state.error}</div>}
+          <button onClick={this.startRequest}>Start Request</button>
+          <button onClick={this.abortRequest}>Abort Request</button>
+          <button onClick={this.reload}>Reload</button>
         </h2>
       </div>
     )
